c72a008d25577fc8b8af8090b0a34ef27dc403d1
FahrizalAziFerdiansyah <47998993+FahrizalAziFerdiansyah@users.noreply.github.com>
2020-07-31 14:06:11 +0700

    sadasd

--- application/views/element/head.php
+++ application/views/element/head.php
@@ -1,53 +1,24 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="zxx">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Imahe Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <meta name="author" content="" />
+    <meta charset="UTF-8">
+    <meta name="description" content="Yoga Studio Template">
+    <meta name="keywords" content="Yoga, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Studio Foto</title>
 
-    <!-- Facebook and Twitter integration -->
-    <meta property="og:title" content="" />
-    <meta property="og:image" content="" />
-    <meta property="og:url" content="" />
-    <meta property="og:site_name" content="" />
-    <meta property="og:description" content="" />
-    <meta name="twitter:title" content="" />
-    <meta name="twitter:image" content="" />
-    <meta name="twitter:url" content="" />
-    <meta name="twitter:card" content="" />
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
-    <link href="<?= base_url() ?>https://fonts.googleapis.com/css?family=Josefin+Sans:100,300,400,600,700" rel="stylesheet">
-    <link href="<?= base_url() ?>https://fonts.googleapis.com/css?family=Amatic+SC:400,700" rel="stylesheet">
-
-    <!-- Animate.css -->
-    <link rel="stylesheet" href="<?= base_url() ?>vendor/studio/css/animate.css">
-    <!-- Icomoon Icon Fonts-->
-    <link rel="stylesheet" href="<?= base_url() ?>vendor/studio/css/icomoon.css">
-    <!-- Bootstrap  -->
-    <link rel="stylesheet" href="<?= base_url() ?>vendor/studio/css/bootstrap.css">
-
-    <!-- Magnific Popup -->
-    <link rel="stylesheet" href="<?= base_url() ?>vendor/studio/css/magnific-popup.css">
-
-    <!-- Owl Carousel -->
-    <link rel="stylesheet" href="<?= base_url() ?>vendor/studio/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="<?= base_url() ?>vendor/studio/css/owl.theme.default.min.css">
-
-    <!-- Theme style  -->
-    <link rel="stylesheet" href="<?= base_url() ?>vendor/studio/css/style.css">
-
-    <!-- Modernizr JS -->
-    <script src="<?= base_url() ?>vendor/studio/js/modernizr-2.6.2.min.js"></script>
-    <!-- FOR IE9 below -->
-    <!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
-
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="<?= base_url() ?>vendor/studio/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="<?= base_url() ?>vendor/studio/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="<?= base_url() ?>vendor/studio/css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="<?= base_url() ?>vendor/studio/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="<?= base_url() ?>vendor/studio/css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="<?= base_url() ?>vendor/studio/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="<?= base_url() ?>vendor/studio/css/style.css" type="text/css">
 </head>
-
-<body>
